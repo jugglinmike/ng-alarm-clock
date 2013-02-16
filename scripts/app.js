27a7211a843f9ef9fe890b1b8be620e56fdeeb48
Mike Pennisi <mike@mikepennisi.com>
2013-02-16 12:22:45 -0500

    Use template filter to display current time
    This represents a stronger separation of concerns since the controller
    should not contain logic for formatting data.

--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,7 @@
 function ClockCtrl($scope, $timeout) {
-	$scope.time = {};
-
-	$scope.formatTimePart = function(value) {
-		var valueStr = value.toString();
-		if (value < 10) {
-			valueStr = "0" + valueStr;
-		}
-		return valueStr;
-	};
 
 	$scope.resetTime = function() {
-		var now = new Date();
-		$scope.time = {
-			hour: now.getHours(),
-			minute: now.getMinutes(),
-			second: now.getSeconds()
-		};
-		angular.forEach($scope.time, function(value, attr) {
-			$scope.time[attr] = $scope.formatTimePart(value);
-		});
+		$scope.time = new Date();
 	};
 
 	var update = function() {
--- test/unit/ClockCtrl.js
+++ test/unit/ClockCtrl.js
@@ -8,14 +8,8 @@ describe("ClockCtrl", function() {
 		ctrl = $controller(ClockCtrl, { $scope: scope });
 	}));
 
-	it("should create a 'time' model", function() {
-		expect(scope.time).not.toBeUndefined();
+	it("should create a 'time' model containing a Date", function() {
+		expect(angular.isDate(scope.time)).toBe(true);
 	});
 
-	it("should update the hour, minute, and second of the 'time' model", function() {
-		scope.resetTime();
-		expect(typeof scope.time.hour).toEqual("string");
-		expect(typeof scope.time.minute).toEqual("string");
-		expect(typeof scope.time.second).toEqual("string");
-	});
 });
